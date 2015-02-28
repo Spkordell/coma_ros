c95da6df024dda91c34ec6a7a5acb9c8eb4544b4
spkordell <spkordell@gmail.com>
2015-02-28 17:59:56 -0500

    Changed leg colors

--- coma_rviz/src/visualization.cpp
+++ coma_rviz/src/visualization.cpp
@@ -2,7 +2,7 @@
 
 visualization::visualization() {
 
-	rod_pos_sub = n.subscribe<coma_rviz::vis>("/rod_pos", 10, &visualization::rodposCallback,this);
+	rod_pos_sub = n.subscribe < coma_rviz::vis > ("/rod_pos", 10, &visualization::rodposCallback, this);
 	marker_pub = n.advertise < visualization_msgs::Marker > ("visualization_marker", 10);
 
 	ROS_INFO("Visualization Node Started");
@@ -12,7 +12,7 @@ void visualization::rodposCallback(const coma_rviz::vis::ConstPtr& msg) {
 	visualization_msgs::Marker points, leg[12], top_plate, mid_plate;
 	points.header.frame_id = top_plate.header.frame_id = mid_plate.header.frame_id = "/base";
 	points.header.stamp = top_plate.header.stamp = mid_plate.header.stamp = ros::Time::now();
-	points.ns = "model legs";
+	points.ns = "points";
 	top_plate.ns = "top plate";
 	mid_plate.ns = "bottom plate";
 	points.action = top_plate.action = mid_plate.action = visualization_msgs::Marker::ADD;
@@ -23,24 +23,20 @@ void visualization::rodposCallback(const coma_rviz::vis::ConstPtr& msg) {
 	points.type = visualization_msgs::Marker::POINTS;
 	top_plate.type = mid_plate.type = visualization_msgs::Marker::LINE_STRIP;
 	// POINTS markers use x and y scale for width/height respectively
-	points.scale.x = 0.005;
-	points.scale.y = 0.005;
-	top_plate.scale.x = 0.005;
-	mid_plate.scale.x = 0.005;
-	// Points are green
-	points.color.g = 1.0f;
+	points.scale.x = 0.002;
+	points.scale.y = 0.002;
+	top_plate.scale.x = 0.002;
+	mid_plate.scale.x = 0.002;
+	// Points are black
 	points.color.a = 1.0;
 	//plates are black
 	mid_plate.color.a = 1.0;
-	mid_plate.color.r = 1.0;
-
 	top_plate.color.a = 1.0;
-	top_plate.color.r = 1.0;
 
 	for (unsigned int i = 0; i < 12; i++) {
 		leg[i].header.frame_id = points.header.frame_id;
 		leg[i].header.stamp = points.header.stamp;
-		leg[i].ns = points.ns;
+		leg[i].ns = "leg " + boost::lexical_cast < std::string > (int(i));
 		leg[i].action = points.action;
 		leg[i].pose.orientation.w = points.pose.orientation.w;
 
@@ -48,12 +44,26 @@ void visualization::rodposCallback(const coma_rviz::vis::ConstPtr& msg) {
 		leg[i].type = visualization_msgs::Marker::LINE_STRIP;
 
 		//LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
-		leg[i].scale.x = 0.005;
+		leg[i].scale.x = 0.002;
 
 		//legs are blue
 		leg[i].color.a = 1.0;
-		leg[i].color.b = 1.0;
+
 	}
+
+	//set leg colors
+	leg[0].color.r = 1.0;leg[0].color.g = 0.0;leg[0].color.b = 0.0;    //red
+	leg[1].color.r = 0.0;leg[1].color.g = 0.0;leg[1].color.b = 1.0;    //blue
+	leg[2].color.r = 0.0;leg[2].color.g = 1.0;leg[2].color.b = 0.0;    //green
+	leg[3].color.r = 1.0;leg[3].color.g = 0.0;leg[3].color.b = 1.0;    //magenta
+	leg[4].color.r = 0.0;leg[4].color.g = 1.0;leg[4].color.b = 1.0;    //cyan
+	leg[5].color.r = 1.0;leg[5].color.g = 1.0;leg[5].color.b = 0.0;    //yellow
+	leg[6].color.r = 1.0;leg[6].color.g = 0.0;leg[6].color.b = 0.0;    //red
+	leg[7].color.r = 0.0;leg[7].color.g = 0.0;leg[7].color.b = 1.0;    //blue
+	leg[8].color.r = 0.0;leg[8].color.g = 1.0;leg[8].color.b = 0.0;    //green
+	leg[9].color.r = 1.0;leg[9].color.g = 0.0;leg[9].color.b = 1.0;    //magenta
+	leg[10].color.r = 0.0;leg[10].color.g = 1.0;leg[10].color.b = 1.0; //cyan
+	leg[11].color.r = 1.0;leg[11].color.g = 1.0;leg[11].color.b = 0.0; //yellow
 
 	// Create the vertices for the points and lines
 	for (unsigned int i = 0; i < 12; i++) {
